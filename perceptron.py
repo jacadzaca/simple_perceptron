47f9a6de9258e0e182166a672b167f7ff39df586
jaca <vitouejj@gmail.com>
2024-11-20 20:51:09 +0100

    add link to iris.csv

--- perceptron.py
+++ perceptron.py
@@ -61,6 +61,7 @@ class Perceptron:
 
 
 def main():
+    # https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv
     with open('iris.csv') as csv_file:
         reader = csv.reader(
             csv_file,
